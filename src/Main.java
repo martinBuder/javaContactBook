41402ac66ddb38a1c4c50e7e591cbdf0ed5b8bc0
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-04-23 16:26:42 +0200

    file crud

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        File file = new File("text.txt");
+        System.out.println(file.exists()); // file.exists() fragt ab, ob Datei existiert
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Fehler");
         }
+
+        //in die Datei schreiben
+        try {
+            FileWriter writer = new FileWriter(file);
+            writer.write("Hei ich schreibe einen Text.");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Fehler");
+        }
+
+        // datei lesen
+        try {
+          FileReader reader = new FileReader(file);
+          int zeichen = 0;
+          while (zeichen != -1) {
+              zeichen = reader.read();
+              System.out.print((char) zeichen);
+          };
+          reader.close();
+        } catch (IOException e) {
+            System.out.println("Fehler");
+        }
+
     }
 }
